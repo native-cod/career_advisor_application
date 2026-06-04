36a836ebf58eb5c607474853b75da9b4015195be
native-cod <110670430+native-cod@users.noreply.github.com>
2026-06-04 08:35:05 +0300

    Changed icon

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -96,7 +96,7 @@ export function Header() {
             <DropdownMenuTrigger asChild>
               <Button variant="ghost" className="relative h-10 w-10 rounded-full">
                 <Avatar className="h-10 w-10">
-                  <AvatarFallback>{getInitials(user.name)}</AvatarFallback>
+                  <AvatarFallback>{getInitials("User")}</AvatarFallback>
                 </Avatar>
               </Button>
             </DropdownMenuTrigger>
